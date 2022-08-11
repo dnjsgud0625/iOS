2c22ab97ee2c85cb562ae84e5db46176870ea6b5
dnjsgud0625 <dnjsgud0625@naver.com>
2022-08-11 18:37:20 +0900

    let,var

---
+++ letvar.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//변수
+var str = "Hello, playground"
+
+var myAge = 10
+
+myAge = 20
+myAge = 50
+
+print(myAge)
+
+
+//Swift 언어 특징
+//Safe Type 타입이 안전함, 타입이 엄격, 강타입
+//입력된 값에 따라 타입이 정해짐, 다른 타입의 값을 입력 시 오류발생
+
+myAge = "hi"
+str = 11
+
+//상수 값이 변하지 않음
+let cnt = 5
+cnt = 11
+
+//상수의 경우 값 변경 불가
+//변수만 값 변경 가능
