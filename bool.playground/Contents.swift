c22e46a1441f281a72363312a4c98abcc4df48ea
dnjsgud0625 <dnjsgud0625@naver.com>
2022-08-23 20:26:34 +0900

    Booleans

---
+++ bool.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+//Booleans
+
+var isOpen = false
+
+if isOpen == false{
+    print("false")
+}else{
+    print("true")
+}
+
+if !isOpen == true{
+    print("true")
+}
+else{
+    print("false")
+}
+
