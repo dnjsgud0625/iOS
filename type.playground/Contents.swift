df5e2c8ce311f87cdc7877526f00d74beb2ee8ab
dnjsgud0625 <dnjsgud0625@naver.com>
2022-08-23 20:21:33 +0900

    Type Annotations

---
+++ type.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// Type Annotations
+
+var greeting: String = "Hello, playground"
+var Age = 14
+
+var screenHeight: Float = 560 //소숫점 사용 가능
+
+//var sum = Age + screenHeight -> int와 float이므로 에러
+var sum = Age + Int(screenHeight)
+
+
