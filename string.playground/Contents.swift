836088ed348ac31cd39f2c8a5257403d2f533d99
dnjsgud0625 <dnjsgud0625@naver.com>
2022-08-27 22:14:14 +0900

    String

--- string.playground/Contents.swift
+++ string.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//String
+
+let name = "AA BB CC"
+
+for i in name{
+    print(i)
+}
+
+let longname =
+"""
+hi
+my
+name
+is
+""" //엔터 안치면 에러
+
+print(longname)
+
+let isOn = false
+isOn.description //description을 통해 string이 아닌것을 string으로 출력
+print(type(of: isOn.description)) //String으로 변환
+
+let number = 123
+String(number)
+
+print("my number is \(number)")
+"my number is \(number)".last
+print("my number is \(number)".last!)
+
+let A = 12.33
+String(A).split(separator: ".")
+
+A.description
+print(A)
