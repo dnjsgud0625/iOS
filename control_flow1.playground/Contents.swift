561f07588feae284c789abe24ff7889b86b3cbbf
dnjsgud0625 <dnjsgud0625@naver.com>
2022-08-30 21:49:58 +0900

    for문

---
+++ control_flow1.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+ //Control Flow(흐름제어)
+ //for문
+
+ let player = "Jordan"
+
+ for ch in player {
+     print(ch)
+ }
+
+ let num = "2468"
+
+ for n in num {
+     print(Int(String(n))!*10)
+ }
+
+ let arr1 = ["Car","Fish","Milk"]
+
+ for item in arr1{
+     print(item)
+ }
+
+ let numoflegs = ["Ant":6,"Dog":4,"Human":2]
+
+ for element in numoflegs{
+     print(element)
+ }
+
+ for i in 0..<5{
+     for j in 0..<5{
+         if i>=j{
+             print("*",terminator: "") //줄바꿈없는 print
+         }
+     }
+     print()
+ }
