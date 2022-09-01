9d6b871b6574544c8dd84e0492805aaaf5d26f2b
dnjsgud0625 <dnjsgud0625@naver.com>
2022-09-01 23:01:52 +0900

    App Start

--- Optional3.playground/Contents.swift
+++ Optional3.playground/Contents.swift
@@ -1,3 +1,50 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Optionals
+//값이 있을수도 있고, 없을수도 있다.
+
+var a: Int? = 30
+var b: Int?
+
+//Unwrapping
+//if statement
+
+if a != nil {
+    print(a!) // 값이 무조건 있기 떄문에 force unwrapping 가능
+}
+
+//if let
+//if var
+
+if let hasNumber = a{ //let이므로 값 변경 불가
+    print(hasNumber)
+}
+
+if var hasNumber2 = a{
+    hasNumber2 = hasNumber2*2
+    print(hasNumber2)
+}
+
+//guard let
+//guard var
+//값이 없을 경우 return 뒤에 내용을 모두 실행시키지 않겠다.
+func testfunc() {
+    guard let hasNumber3 = a else{ //-> hasNumber3에 값이 없을 경우에는 return 아래 로직들이 실행되지 않는다.
+        return
+    }
+    print(hasNumber3)
+    print("end")
+}
+
+testfunc()
+
+func testfunc2() {
+    guard var hasNumber4 = a else{
+        return
+    }
+    hasNumber4 = hasNumber4*4
+    print(hasNumber4)
+    print("end")
+}
+
+testfunc2()
--- optional4.playground/Contents.swift
+++ optional4.playground/Contents.swift
@@ -13,6 +13,15 @@ A.last
 type(of: A.last)
 print(A.last)
 
+
+
+
+
+
+
+
+
+
 //왜 last를 하면 optional 타입으로 출력이 되는가?
 //혹사 A.last는 chr타입이고, A는 String이라서 강타입인 스위프트에서 다른 타입에 대한 충돌을 막기위해서인가?
 
